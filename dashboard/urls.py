6f0f6e7671db52e770124d04de80654caac3c0b0
Jasmeet <js3393187@gmail.com>
2023-03-23 09:40:14 +0530

    Route for creating new user

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-
 from . import views
 
 urlpatterns = [
     path('admin/', views.admin_page, name='dashboard-admin'),
+    path('admin/create-user', views.create_new_user, name='admin-createuser'),
     path('admin/PriceList/', views.pricelist_view, name='pricelist-view'),
     path('distributer/', views.distributer_page, name='dashboard-distributer'),
     path('retailer/', views.retailer_page, name='dashboard-retailer'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
