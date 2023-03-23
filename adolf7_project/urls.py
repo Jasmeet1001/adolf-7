09fb69b2375c878f0448910c16cf4a53a51be4ca
Jasmeet <js3393187@gmail.com>
2023-03-23 09:30:55 +0530

    User Uploaded Static Files Deployment

--- adolf7_project/urls.py
+++ adolf7_project/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('register.urls')),
     path('logout/', auth_views.LogoutView.as_view(template_name='register/logout.html'), name='logout-view'),
     path('dashboard/', include('dashboard.urls')),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
