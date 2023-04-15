dd4ce4aff93e14efc95fac225e5f1bc8007383f9
Jasmeet <js3393187@gmail.com>
2023-04-15 18:28:29 +0530

    changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', views.admin_page, name='dashboard-admin'),
-    path('admin/create-user', views.create_new_user, name='admin-createuser'),
+    path('admin/create-user/', views.create_new_user, name='admin-createuser'),
     path('admin/pricelist/', views.order_view_admin, name='order-view-admin'),
     path('distributor/', views.distributor_page, name='dashboard-distributor'),
-    path('distributor/pricelist', views.order_view_distributor, name='order-view-distributor'),
+    path('distributor/pricelist/', views.order_view_distributor, name='order-view-distributor'),
     path('retailer/', views.retailer_page, name='dashboard-retailer'),
     path('retailer/pricelist', views.order_view_retailer, name='order-view-retailer'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -42,7 +42,6 @@ def create_new_user(request):
                 if option == '1':
                     admin_form = NewAdmin(request.POST)
                     if admin_form.is_valid() and user_form.is_valid():
-                        # Distributer.objects.create()
                         temp = admin_form.save(commit=False)
                         temp.user = user
                         temp.save()
@@ -51,15 +50,7 @@ def create_new_user(request):
 
                 if option == '2':
                     dist_form = NewDist(request.POST, instance=request.user)
-                    # user_form = UserCreationForm(request.POST)
                     if dist_form.is_valid() and user_form.is_valid():
-                        # pnum = user_form.cleaned_data.get('phone_number')
-                        # fnm = user_form.cleaned_data.get('first_name')
-                        # lnm = user_form.cleaned_data.get('last_name')
-                        # email = user_form.cleaned_data.get('email')
-                        # pwd = user_form.cleaned_data.get('password')
-                        # user = User.objects.create_user(phone_number=pnum, password=pwd, first_name=fnm, last_name=lnm, email=email)
-                        # Distributer.objects.create()
                         temp = dist_form.save(commit=False)
                         temp.user = user
                         temp.save()
@@ -67,15 +58,7 @@ def create_new_user(request):
 
                 elif option == '3':
                     ret_form = NewRet(request.POST)
-                    # user_form = UserCreationForm(request.POST)
                     if ret_form.is_valid() and user_form.is_valid():
-                        # pnum = user_form.cleaned_data.get('phone_number')
-                        # fnm = user_form.cleaned_data.get('first_name')
-                        # lnm = user_form.cleaned_data.get('last_name')
-                        # email = user_form.cleaned_data.get('email')
-                        # pwd = user_form.cleaned_data.get('password')
-                        # user = User.objects.create_user(phone_number=pnum, password=pwd, first_name=fnm, last_name=lnm, email=email)
-                        # Distributer.objects.create()
                         temp = ret_form.save(commit=False)
                         temp.user = user
                         temp.save()
@@ -101,6 +84,8 @@ def admin_page(request):
     retailers_all = []
     for distributer in distributers_all:
         retailers_all.extend(distributer.retailers.all())
+    
+    print(retailers_all)
 
     context = {
         'dist_obj': distributers_all,
@@ -111,8 +96,8 @@ def admin_page(request):
 
 @login_required
 def order_view_admin(request):
-    pricelist_all = PriceList.objects.filter(id__lt=190)
-    # pricelist_all = PriceList.objects.all()
+    # pricelist_all = PriceList.objects.filter(id__lt=190)
+    pricelist_all = PriceList.objects.all()
     
     context = {
         'pricelist_obj': pricelist_all,
